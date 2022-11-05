3a9a333d7138c57d4a26ac545c49f108ef7e7776
Dallin Johnson <dallin.coder@gmail.com>
2022-11-05 16:14:02 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # capstone
 # capstoneComputerShop
+# new-portfolio
--- server/server.js
+++ server/server.js
@@ -20,13 +20,25 @@ app.delete('/api/data/:id', deleteData)
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../home.html'));
+  res.sendFile(path.join(__dirname, '../templates/resume.html'));
 });
-app.get('/home.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '../home.html'));
+app.get('/fun.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/fun.html'));
+});
+app.get('/references.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/references.html'));
+});
+app.get('/resume.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/resume.html'));
+});
+app.get('/experience.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/experience.html'));
+});
+app.get('/contact.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/contact.html'));
 });
 app.get('axios', (req, res) => {
-  res.sendFile(path.join(__dirname, '/axios'));
+  res.sendFile(path.join(__dirname, '../axios'));
 });
 app.get('../index.css', (res,req) => {
   res.sendFile(path.join(__dirname, '../index.css'));
@@ -35,34 +47,37 @@ app.get('/index.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.js'));
 });
 app.get('/controller.js', (res,req) => {
-  res.sendFile(path.join(__dirname, './controller.js'));
+  res.sendFile(path.join(__dirname, './templates/controller.js'));
 });
 app.get('/db.json', (res,req) => {
-  res.sendFile(path.join(__dirname, './db.json'));
+  res.sendFile(path.join(__dirname, './templates/db.json'));
 });
-app.get('/parts.html', (req, res) => {
-  res.sendFile(path.join(__dirname , '../parts.html'));
+app.get('/about.html', (res,req) => {
+  res.sendFile(path.join(__dirname, '../templates/about.html'));
+});
+app.get('/projects.html', (req, res) => {
+  res.sendFile(path.join(__dirname , '../templates/projects.html'));
 });
 app.get('/pay.html', (req, res) => {
-  res.sendFile(path.join(__dirname , '../pay.html'));
+  res.sendFile(path.join(__dirname , '../templates/pay.html'));
 });
 app.get('/pc.html', (req, res) => {
-  res.sendFile(path.join(__dirname , '../pc.html'));
+  res.sendFile(path.join(__dirname , '../templates/pc.html'));
 });
 app.get('/phones.html', (req, res) => {
-  res.sendFile(path.join(__dirname , '../phones.html'));
+  res.sendFile(path.join(__dirname , '../templates/phones.html'));
 });
 app.get('/processors.html', (req, res) => {
-  res.sendFile(path.join(__dirname , '../processors.html'));
+  res.sendFile(path.join(__dirname , '../templates/processors.html'));
 });
 app.get('/sell.html', (req, res) => {
-  res.sendFile(path.join(__dirname , '../sell.html'));
+  res.sendFile(path.join(__dirname , '../templates/sell.html'));
 });
 app.get('/vr.html', (req, res) => {
-  res.sendFile(path.join(__dirname , '../vr.html'));
+  res.sendFile(path.join(__dirname , '../templates/vr.html'));
 });
-app.get('/checkout.html', (req, res) => {
-  res.sendFile(path.join(__dirname , '../checkout.html'));
+app.get('/interests.html', (req, res) => {
+  res.sendFile(path.join(__dirname , '../templates/interests.html'));
 });
 app.use(express.static("."));
 
